4b4e6241da2692a95b7394475c71442193361a19
kagg-design <info@kagg.eu>
2020-06-27 15:35:14 +0300

    Get rid of outdated code, follow modern login.php.

--- theme/password-protected-login.php
+++ theme/password-protected-login.php
@@ -1,8 +1,7 @@
 <?php
 
 /**
- * Based roughly on wp-login.php @revision 19414
- * http://core.trac.wordpress.org/browser/trunk/wp-login.php?rev=19414
+ * Based roughly on wp-login.php
  */
 
 global $wp_version, $Password_Protected, $error, $is_iphone;
@@ -12,17 +11,9 @@ global $wp_version, $Password_Protected, $error, $is_iphone;
  */
 if ( ! function_exists( 'wp_shake_js' ) ) {
 	function wp_shake_js() {
-		global $is_iphone;
-		if ( $is_iphone ) {
-			return;
-		}
 		?>
-		<script>
-		addLoadEvent = function(func){if(typeof jQuery!="undefined")jQuery(document).ready(func);else if(typeof wpOnload!='function'){wpOnload=func;}else{var oldonload=wpOnload;wpOnload=function(){oldonload();func();}}};
-		function s(id,pos){g(id).left=pos+'px';}
-		function g(id){return document.getElementById(id).style;}
-		function shake(id,a,d){c=a.shift();s(id,c);if(a.length>0){setTimeout(function(){shake(id,a,d);},d);}else{try{g(id).position='static';wp_attempt_focus();}catch(e){}}}
-		addLoadEvent(function(){ var p=new Array(15,30,15,0,-15,-30,-15,0);p=p.concat(p.concat(p));var i=document.forms[0].id;g(i).position='relative';shake(i,p,20);});
+		<script type="text/javascript">
+			document.querySelector('form').classList.add('shake');
 		</script>
 		<?php
 	}
@@ -56,7 +47,7 @@ if ( isset( $_POST['password_protected_cookie_test'] ) && empty( $_COOKIE[ TEST_
 // Shake it!
 $shake_error_codes = array( 'empty_password', 'incorrect_password' );
 if ( $Password_Protected->errors->get_error_code() && in_array( $Password_Protected->errors->get_error_code(), $shake_error_codes ) ) {
-	add_action( 'password_protected_login_head', 'wp_shake_js', 12 );
+	add_action( 'login_footer', 'wp_shake_js', 12 );
 }
 
 // Obey privacy setting
@@ -74,32 +65,7 @@ add_action( 'password_protected_login_head', 'wp_login_viewport_meta' );
 
 <?php
 
-if ( version_compare( $wp_version, '3.9-dev', '>=' ) ) {
-	wp_admin_css( 'login', true );
-} else {
-	wp_admin_css( 'wp-admin', true );
-	wp_admin_css( 'colors-fresh', true );
-}
-
-?>
-
-<style media="screen">
-#login_error, .login .message, #loginform { margin-bottom: 20px; }
-</style>
-
-<?php
-
-if ( $is_iphone ) {
-	?>
-	<meta name="viewport" content="width=320; initial-scale=0.9; maximum-scale=1.0; user-scalable=0;" />
-	<style media="screen">
-	.login form, .login .message, #login_error { margin-left: 0px; }
-	.login #nav, .login #backtoblog { margin-left: 8px; }
-	.login h1 a { width: auto; }
-	#login { padding: 20px 0; }
-	</style>
-	<?php
-}
+wp_admin_css( 'login', true );
 
 do_action( 'login_enqueue_scripts' );
 do_action( 'password_protected_login_head' );
